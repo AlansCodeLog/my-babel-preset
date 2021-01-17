939a6e2ead05c4806dcc1ab981f5ff36b3f190a1
Alan <alanscodelog@gmail.com>
2021-01-16 21:01:34 -0300

    style: changed whitespace/quotes style

--- babel.config.js
+++ babel.config.js
@@ -1,33 +1,32 @@
-
 module.exports = function (api) {
 	api.cache(true)
 	return {
-		"presets": [
-			[ "@babel/typescript", {
-				"onlyRemoveTypeImports": true,
+		presets: [
+			["@babel/typescript", {
+				onlyRemoveTypeImports: true,
 				// parses js files as typescript
 				// not for actually writing typescript in js files (IDEs should complain anyways), but useful for allowing modern syntax in js files
-				"allExtensions": true,
-				"allowDeclareFields": true,
-			} ],
-			[ "@babel/env", {
-				"bugfixes": true,
-				"useBuiltIns": "usage",
-				"corejs": "3.0.0",
-				// target should be set by using browserlist in the package.json
-			} ]
+				allExtensions: true,
+				allowDeclareFields: true,
+			}],
+			["@babel/env", {
+				bugfixes: true,
+				useBuiltIns: "usage",
+				corejs: "3.0.0",
+				// target should be set by using browserslist in the package.json
+			}],
 		],
-		"plugins": [
-			[ "module-resolver", {
-				"root": [ "../" ],
-				"alias": {
+		plugins: [
+			["module-resolver", {
+				root: ["../"],
+				alias: {
 					"@": "./src",
 					"@tests": "./tests",
 					"@utils": "@alanscodelog/utils/dist",
-				}
-			} ],
+				},
+			}],
 			"@babel/plugin-proposal-class-properties",
-			"@babel/plugin-proposal-export-namespace-from"
-		],
+			"@babel/plugin-proposal-export-namespace-from",
+		]
 	}
 }
